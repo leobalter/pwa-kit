d95859d068184baef90abc107da3d360d985766b
Adam Raya Navarro <arayanavarro@salesforce.com>
2022-05-05 18:49:51 -0700

    Update create-mobify-app-dev.js

--- packages/pwa-kit-create-app/scripts/create-mobify-app-dev.js
+++ packages/pwa-kit-create-app/scripts/create-mobify-app-dev.js
@@ -121,7 +121,6 @@ const withLocalNPMRepo = (func) => {
 
                                     readStream.on('error', (err) => {
                                         console.log('LogFile: ERROR READING err:', err)
-
                                         waitForLogFileExists()
                                     })
                                 }
